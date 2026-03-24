9c294ea931ecf544034f10f15ec8574b81632dca
venu <venu46257@gmail.com>
2026-03-24 14:58:20 -0400

    Initial Database config

---
+++ app/core/database.py
@@ -0,0 +1,17 @@
+#DataBase connection and session management
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv() #loads variables from .env
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE URL is not sety in env")
+
+engine=create_engine(DATABASE_URL)
+
+sessionLocal=sessionmaker(autocommit=False, autoflush=False,bind=engine)
+
+Base = declarative_base()
